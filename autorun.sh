79b73d5ddcfdf584d4032e19df0632866898d55a
SamanGhn <141281012+SamanGhn@users.noreply.github.com>
2024-09-26 01:43:49 +0330

    Add files via upload

---
+++ autorun.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# آدرس فایل اسکریپت در GitHub
+SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/BackHaul/blob/main/autoscripts.sh"
+
+# دانلود و اجرای فایل اسکریپت
+bash <(curl -Ls "$SCRIPT_URL")
