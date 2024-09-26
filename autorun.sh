a5eda48e472eb3b048099e6b9d9754508b2b93f5
SamanGhn <141281012+SamanGhn@users.noreply.github.com>
2024-09-26 11:37:22 +0330

    Update autorun.sh

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# آدرس فایل اسکریپت در GitHub
-SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/BackHaul/blob/main/autoscripts.sh"
+# آدرس فایل اسکریپت در GitHub (دسترسی به فایل خام)
+SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/BackHaul/main/autoscripts.sh"
 
 # دانلود و اجرای فایل اسکریپت
 bash <(curl -Ls "$SCRIPT_URL")
