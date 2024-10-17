392b2b2dfcb250db0354bb7a991c47efc1d577f6
SamanGhn <141281012+SamanGhn@users.noreply.github.com>
2024-10-17 10:09:28 +0330

    Delete autorun.sh

--- autorun.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# آدرس فایل اسکریپت در GitHub (دسترسی به فایل خام)
-SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/BackHaul/main/autoscripts.sh"
-
-# دانلود و اجرای فایل اسکریپت
-bash <(curl -Ls "$SCRIPT_URL")
