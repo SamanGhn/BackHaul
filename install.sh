104e137dc35f363ff61dbb662379a4daaa218095
SamanGhn <141281012+SamanGhn@users.noreply.github.com>
2024-10-18 09:37:15 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,48 @@
 #!/bin/bash
 
-# آدرس فایل اسکریپت در GitHub
-SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/BackHaul/main/setup.sh"
+# تابع برای نمایش منو
+show_menu() {
+  echo "1) Tcp Configuration"
+  echo "2) WS Configuration"
+  echo "3) Exit"
+}
 
-# دانلود و اجرای فایل اسکریپت
-bash <(curl -Ls "$SCRIPT_URL")
+# تابع برای اجرای TCP Configuration
+run_tcp_configuration() {
+  SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/BackHaul/main/tcp.sh"
+  echo "Running TCP Configuration..."
+  bash <(curl -Ls "$SCRIPT_URL")
+}
+
+# تابع برای اجرای WS Configuration
+run_ws_configuration() {
+  SCRIPT_URL="https://raw.githubusercontent.com/SamanGhn/BackHaul/main/ws.sh"  # فرض می‌کنیم اسکریپت مربوط به WS اینجا موجود است
+  echo "Running WS Configuration..."
+  bash <(curl -Ls "$SCRIPT_URL")
+}
+
+# تابع برای خروج
+exit_program() {
+  echo "Exiting..."
+  exit 0
+}
+
+# حلقه اصلی
+while true; do
+  show_menu
+  read -p "Choose an option: " choice
+  case $choice in
+    1)
+      run_tcp_configuration
+      ;;
+    2)
+      run_ws_configuration
+      ;;
+    3)
+      exit_program
+      ;;
+    *)
+      echo "Invalid option. Please try again."
+      ;;
+  esac
+done
